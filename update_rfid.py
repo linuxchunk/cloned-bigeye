0ea9e6210de889870658455cc432bceb6d6ecc01
Ramesh Patel <rameshpatel.d1516@gmail.com>
2022-11-11 19:13:32 +0530

    Update update_rfid.py

--- update_rfid.py
+++ update_rfid.py
@@ -3,10 +3,11 @@ import requests
 import json
 import time
 import pickle
+import pandas as pd
  
 d = {}
-
-url_post="http://34.28.3.109:8080/updaterfid"
+dic = {}
+url_post="http://34.28.3.109:80/updaterfid"
 headers = {
     "Content-Type": "application/json; charset=utf-8",
     "x-hasura-admin-secret": "4E9fBl6pQoyEL138Ov9jmoY3xnKtMpKm2KtrHWHPOUdcXzMHBzvII9CDooZZH5Ay",
@@ -16,8 +17,11 @@ def register(dict):
     return requests.patch(url_post, headers=headers, json=dict)
  
 
-while True:
-    rfid = input("Register your RFID Enabled Card")
-    email = input("Enter your email: ")
-    print(register({"rfid_key":rfid,"email":email}))
 
+    #rfid = input("Register your RFID Enabled Card")
+data = pd.read_csv(r'/home/ramesh/Downloads/KGX Final RFID - October.csv')
+emails = data.loc[:,['email','rfid']]
+for email,rfids in emails:
+    print(register({"rfid_key":rfids,"email":email}))
+        
+    #email = input("Enter your email: ")
